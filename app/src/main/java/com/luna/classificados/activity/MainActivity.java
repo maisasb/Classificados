84c470b569ef5152248dc17a224b1e83d6a2e8e3
Maisa <maisasb15@gmail.com>
2018-01-17 19:37:17 -0200

    Configuração inicial do projeto

---
+++ app/src/main/java/com/luna/classificados/activity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.luna.classificados.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.luna.classificados.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
