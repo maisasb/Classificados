40fc5c3cec88b3e4a6b407b00a7f4d31e43da4f2
nataliavaz <nvs@2308>
2018-01-24 22:03:22 -0200

    Estilo Botao

--- app/src/main/java/com/luna/classificados/activity/MainActivity.java
+++ app/src/main/java/com/luna/classificados/activity/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_conf, menu);
         return true;
@@ -49,16 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.item_sair) {
-            logoutApp();
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                logoutApp();
+                return true;
+            default: return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
